1ce02d792070d6bdf9c6393e83b52d2c2bf09373
hithaishi <hithaishial2007@gmail.com>
2026-07-25 22:33:25 +0530

    largest element

--- DSA problems/find_largest_element.js
+++ DSA problems/find_largest_element.js
@@ -1,12 +1,40 @@
-let oddNumbers = [75, 31, 89, 43, 29]
-let largest = oddNumbers[0];
+function largestElement(){
 
-for(let i = 1; i < oddNumbers.length; i++){
+    let oddNumbers = [75, 31, 89, 43, 29]
+    let largest = oddNumbers[0];
 
-    if(oddNumbers[i] > largest){
-
-        largest = oddNumbers[i];
+    for(let i = 1; i < oddNumbers.length; i++){
+        
+        if(oddNumbers[i] > largest){
+            
+            largest = oddNumbers[i];
+        }
     }
+    
+    console.log("the largest element is: ", largest);
 }
 
-console.log("the largest element is: ", largest);
+function secondLargestElement(){
+
+    let oddNumbers = [75, 31, 89, 43, 29]
+    let largest = oddNumbers[0];
+    let secondLargest;
+
+    for(let i = 0; i < oddNumbers.length; i++){
+
+        if(oddNumbers[i] >= largest){
+
+            secondLargest = largest;
+            largest = oddNumbers[i];
+            
+        }else if (oddNumbers[i] > secondLargest && oddNumbers[i] < largest) {
+            
+            secondLargest = oddNumbers[i];
+        }
+    }
+
+    console.log("The second largest number is: ", secondLargest);
+}
+
+largestElement();
+secondLargestElement();
