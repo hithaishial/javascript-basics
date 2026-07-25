fa736686197294d9692b7be205d8eb900a3c0c32
hithaishi <hithaishial2007@gmail.com>
2026-07-25 23:23:32 +0530

    smallest element

--- DSA problems/find_largest_element.js
+++ DSA problems/find_largest_element.js
@@ -58,7 +58,22 @@ function secondSmallestElement() {
     console.log("The second smallest number is: ", secondSmallest);
 }
 
+function smallestElement(){
 
+let numbers1 = [39, 98, 654, 16, 82]
+let smallest = numbers1[0];
+
+for(let i=1; i<numbers1.length; i++){
+
+    if(smallest > numbers1[i]){
+        smallest = numbers1[i];
+    }
+
+}
+console.log("the smallest element is: ", smallest);
+}
+
+smallestElement();
 largestElement();
 secondLargestElement();
 secondSmallestElement();
--- DSA problems/find_smallest_element.js
+++ DSA problems/find_smallest_element.js
@@ -1,16 +1,1 @@
-function smallestElement(){
 
-let numbers1 = [39, 98, 654, 16, 82]
-let smallest = numbers1[0];
-
-for(let i=1; i<numbers1.length; i++){
-
-    if(smallest > numbers1[i]){
-        smallest = numbers1[i];
-    }
-
-}
-console.log("the smallest element is: ", smallest);
-}
-
-smallestElement();
