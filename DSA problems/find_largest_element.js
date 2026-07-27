7593ed68b6092968271a1eefecd51237f03dcb80
hithaishi <hithaishial2007@gmail.com>
2026-07-27 22:39:40 +0530

    made changes in func finding smallest element and removed a file link from html file

--- DSA problems/find_largest_element.js
+++ DSA problems/find_largest_element.js
@@ -63,7 +63,7 @@ function smallestElement(){
 let numbers1 = [39, 98, 654, 16, 82]
 let smallest = numbers1[0];
 
-for(let i=1; i<numbers1.length; i++){
+for(let i=1; i < numbers1.length; i++){
 
     if(smallest > numbers1[i]){
 
